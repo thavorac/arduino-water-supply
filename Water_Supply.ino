f5584ef00ce7e370228535ead496c7a082efdb6c
FooNG P.S <liw48@hotmail.com>
2015-09-25 10:34:00 +0700

    Modify some function

--- Water_Supply.ino
+++ Water_Supply.ino
@@ -26,8 +26,17 @@ Servo servo2;
 Servo servo3;
 Servo servo4;
 float temp1, temp2, temp3;
+float C1, C2, C3;
 String msg = "";
 int humidity;
+
+bool isOpenS1 = false;
+bool isOpenS2 = false;
+bool isOpenS3 = false;
+bool isOpenS4 = false;
+bool isOpenAll = false;
+
+bool isAuto;
  
 void setup()
 {
@@ -49,6 +58,15 @@ void setup()
   // Allow the hardware to sort itself out
   delay(5000);
 
+  isAuto = true;
+
+}
+
+void spinServo(Servo servo){
+  servo.write(90);
+  delay(500);
+  servo.write(120);
+  delay(500);
 }
 
 void loop()
@@ -59,12 +77,22 @@ void loop()
   Serial.println("message in loop function: "+msg);
   
   humidity = analogRead(A0);
-  temp1= analogRead(A1);
-  temp2= analogRead(A2);
-  temp3= analogRead(A3);
-  float C1 = (25*temp1-2050)/100.0;
-  float C2 = (25*temp2-2050)/100.0;
-  float C3 = (25*temp3-2050)/100.0;
+
+  temp1 = temp2 = temp3 = 0;
+  for (int i = 0; i<5; i++) {
+    temp1 += analogRead(A1);
+    temp2 += analogRead(A2);
+    temp3 += analogRead(A3);
+  }
+  temp1 /= 5;
+  temp2 /= 5;
+  temp3 /= 5;
+
+  // Convert to celcius
+  C1 = (25*temp1-2050)/100.0;
+  C2 = (25*temp2-2050)/100.0;
+  C3 = (25*temp3-2050)/100.0;
+  
   Serial.print("Degree of field 1 = ");
   Serial.print(C1);
   Serial.print(" C ");
@@ -80,9 +108,9 @@ void loop()
   
 
   // This check the message from mobile
-  if(msg.equals("") || msg.equals("auto")){ // for automatic system
+  if(isAuto){ // for automatic system
     
-   /* if(humidity>350){
+    if(humidity>200){
       Serial.println("The water supply 2 is opening now.");
       servo2.write(60);
       delay(500);
@@ -98,40 +126,64 @@ void loop()
       servo2.write(0);
     }
    // delay(5000);
-    if(C1>=20){
+    if(C1>=25){
       Serial.println("The water supply 1 is opening now.");
-      servo1.write(90);
-      delay(500);
-      servo1.write(120);
-      delay(500);
+      spinServo(servo1);
     }else{
       Serial.println("The water supply 1 is closed.");
       servo1.write(0);
      }
 
     // delay(5000);
-    if(C2>=20){
+    if(C2>=25){
       Serial.println("The water supply 3 is opening now.");
-      servo3.write(90);
-      delay(500);
-      servo3.write(120);
-      delay(500);
+      spinServo(servo3);
     }else{
       Serial.println("The water supply 3 is closed.");
       servo3.write(0);
      }
-    // delay(5000);*/
+    // delay(5000);
     if(C3>=25){
       Serial.println("The water supply 4 is opening now.");
-      servo4.write(90);
-      delay(500);
-      servo4.write(120);
-      delay(500);
+      spinServo(servo4);
     }else{
       Serial.println("The water supply 4 is closed.");
       servo4.write(0);
      }
   }else{ //for manaul system
+    
+    if(isOpenS1){
+      spinServo(servo1);
+      Serial.println("Manual - The water supply 1 is opening now.");
+    } else {
+      servo1.write(0);
+      Serial.println("Manual - All the water supply 1 is closed.");
+    }
+    
+    if(isOpenS2){
+      spinServo(servo2);
+      Serial.println("Manual - The water supply 2 is opening now.");
+    } else{
+      servo2.write(0);
+      Serial.println("Manual - All the water supply 2 is closed.");
+    }
+    
+    if(isOpenS3){
+      spinServo(servo3);
+      Serial.println("Manual - The water supply 3 is opening now.");
+    }else{
+      servo3.write(0);
+      Serial.println("Manual - All the water supply 3 is closed.");
+    }
+    
+    if(isOpenS4){
+      spinServo(servo4);
+      Serial.println("Manual - The water supply 4 is opening now.");
+    } else{
+      servo4.write(0);
+      Serial.println("Manual - All the water supply 4 is closed.");
+    }
+    
   }
   
   client.loop();
@@ -143,74 +195,55 @@ void callback(char* topic, byte* payload, unsigned int length) {
   msg = (char*)payload;
   
   Serial.println("message in callback function: "+msg);
-  
+
+  if(msg.equals("") || msg.equals("auto")){
+    Serial.println("is auto");
+    Serial.println();
+    isAuto = true;
+  } else if (msg.equals("manual")){
+    Serial.println("is manual");
+    Serial.println();
+    isAuto = false;
+  }
+
   if(msg.equals("os1")){
-      servo1.write(90);
-      delay(500);
-      servo1.write(120);
-      delay(500);
-    Serial.println("The water supply 1 is opening now.");
+    isOpenS1 = true;
   }
-  else if(msg.equals("os2")){
-      servo2.write(90);
-      delay(500);
-      servo2.write(120);
-      delay(500);
-    Serial.println("The water supply 2 is opening now.");
+  if(msg.equals("os2")){
+    isOpenS2 = true;
   }
-  else if(msg.equals("os3")){
-      servo3.write(90);
-      delay(500);
-      servo3.write(120);
-      delay(500);
-    Serial.println("The water supply 3 is opening now.");
+  if(msg.equals("os3")){
+    isOpenS3 = true;
   }
-  else if(msg.equals("os4")){
-      servo4.write(90);
-      delay(500);
-      servo4.write(120);
-      delay(500);
-    Serial.println("The water supply 4 is opening now.");
+  if(msg.equals("os4")){
+    isOpenS4 = true;
   }
-  else if(msg.equals("open")){
-    servo1.write(90);
-    servo2.write(90);
-    servo3.write(90);
-    servo4.write(90);
-    delay(500);
-    servo1.write(120);
-    servo2.write(120);
-    servo3.write(120);
-    servo4.write(120);
-    delay(500);
-    Serial.println("All the water supply is opening now.");
+  if(msg.equals("open")){
+    isOpenS1 = true;
+    isOpenS2 = true;
+    isOpenS3 = true;
+    isOpenS4 = true;
   }
-  else if(msg.equals("close")){
-    servo1.write(0);
-    servo2.write(0);
-    servo3.write(0);
-    servo4.write(0);
-    Serial.println("All the water supply is closed.");
+
+  if(msg.equals("cs1")){
+    isOpenS1 = false;
   }
-  else if(msg.equals("cs1")){
-    servo1.write(0);
-    Serial.println("All the water supply 1 is closed.");
+  if(msg.equals("cs2")){
+    isOpenS2 = false;
   }
-  else if(msg.equals("cs2")){
-    servo2.write(0);
-    Serial.println("All the water supply 2 is closed.");
+  if(msg.equals("cs3")){
+    isOpenS3 = false;
   }
-  else if(msg.equals("cs3")){
-    servo3.write(0);
-    Serial.println("All the water supply 3 is closed.");
+  if(msg.equals("cs4")){
+    isOpenS4 = false;
   }
-  else if(msg.equals("cs4")){
-    servo4.write(0);
-    Serial.println("All the water supply 4 is closed.");
+  if(msg.equals("close")){
+    isOpenS1 = false;
+    isOpenS2 = false;
+    isOpenS3 = false;
+    isOpenS4 = false;
   }
-  else{
-    Serial.println("Do nothing"); 
-  }
+  
 }
 
 void reconnect() {
